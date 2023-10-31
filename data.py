bdcd6591831e52a8d99273dd569c299ec068f1ca
qaelena181993 <85900418+qaelena181993@users.noreply.github.com>
2023-10-31 23:13:02 +0300

    file added

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Гоша",
+    "lastName": "Мариевич",
+    "address": "Центральный проезд Хорошёвского Серебряного Бора 2",
+    "metroStation": 204,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2023-09-27",
+    "comment": "Тестируем",
+    "color": [
+        "BLACK"
+    ]
+}
